1c4b40d0f14da9e4c74a0bac5750299922a018e9
Yuan Gao <gao308680162@gmail.com>
2020-08-14 14:31:47 -0400

    fix the bug on the task1, the program compile and run correctly.

--- src/chatgui.h
+++ src/chatgui.h
@@ -29,7 +29,7 @@ public:
 
     // getter / setter
     //ChatLogic *GetChatLogicHandle() { return _chatLogic; }
-    std::unique_ptr<ChatLogic> GetChatLogicHandle() { return std::move(_chatLogic); }
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
     // events
     void paintEvent(wxPaintEvent &evt);
     void paintNow();
